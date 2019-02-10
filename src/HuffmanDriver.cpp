baf17400142828aeb2fdc0c4f68a41398863704f
Austin Rovge <austinrovge@gmail.com>
2019-02-10 02:33:28 -0600

    added a basic method for reading from a file. added test file to test method.

--- src/HuffmanDriver.cpp
+++ src/HuffmanDriver.cpp
@@ -5,8 +5,11 @@
  */
 
 #include <iostream>
+#include <fstream>
 #include "HuffmanLeaf.h"
 #include "HuffmanNode.h"
+
+void readFile();
 
 int main() {
     HuffmanLeaf leaf = HuffmanLeaf(5, 'a');
@@ -19,5 +22,26 @@ int main() {
     // expected 33 = 3 + 5 + 25
     std::cout << "The frequency for this HuffmanNode is: " << node.getFrequency() << std::endl;
 
+    std::cout << std::endl;
+
+    // read file
+    readFile();
+
     return 0;
+}
+
+void readFile() {
+    std::ifstream file;
+    file.open("../src/original.txt");
+
+    std::string line;
+
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            std::cout << line << std::endl;
+        }
+        file.close();
+    } else {
+        std::cout << "Unable to open file" << std::endl;
+    }
 }
