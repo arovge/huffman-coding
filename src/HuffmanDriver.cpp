2c4e2f4f705ffefc5fcc77b1cda637eaebf86e1d
Austin Rovge <austinrovge@gmail.com>
2019-02-09 23:58:20 -0600

    added driver class and cmake file to build

---
+++ src/HuffmanDriver.cpp
@@ -0,0 +1,13 @@
+/*
+ * Project: huffman-coding
+ * Developer: Austin Rovge
+ * Date: 2/10/2019
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
